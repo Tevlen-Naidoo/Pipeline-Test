6687f254060dc4c3db02491981c06bcc0de0c962
Tevlen-Naidoo <tevlen.naidoo@bbd.co.za>
2025-01-20 21:02:48 +0200

    And we are back

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    // res.sendFile(path.join(__dirname, '/index.html'));
+    res.writeHead(200, {"content-type": "text/html"});
+    res.write('Hello World!');
+    res.end();
 });
 
 app.listen(port);
-console.log(`Server started at http://localhost:${port}`);
+// console.log(`Server started at http://localhost:${port}`);
+
